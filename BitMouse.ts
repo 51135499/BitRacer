485ec2a7ab4efa041b4387a67503321d67954454
51135499 <a172839f@gmail.com>
2020-02-13 12:12:19 +0800

    Add files via upload

---
+++ BitMouse.ts
@@ -0,0 +1,119 @@
+const N76_ADDR = 0x10
+
+enum PingUnit {
+    //% block="μs"
+    MicroSeconds,
+    //% block="cm"
+    Centimeters,
+}
+
+//% weight=0 color=#f98020 icon="\uf1ba" block="BitMouse"
+namespace BitMouse {
+    export enum Motors {
+        //% blockId="left motor" block="left"
+        M_R = 0,
+        //% blockId="right motor" block="right"
+        M_L = 1,
+        //% blockId="all motor" block="all"
+        All = 2
+    }
+    export enum IR_Sensors {
+        //% blockId="IR1_Sensors" block="IR1"
+        IR1 = 0x03,
+        //% blockId="IR2_Sensors" block="IR2"
+        IR2 = 0x04,
+        //% blockId="IR3_Sensors" block="IR3"
+        IR3 = 0x05,
+        //% blockId="IR4_Sensors" block="IR4"
+        IR4 = 0x06,
+    }
+	export enum LEDs {
+        //% blockId="LED_right" block="right"
+        LED_R = 8,
+        //% blockId="LED_left" block="left"
+        LED_L = 16
+    }
+	export enum LEDswitch {
+        //% blockId="on" block="on"
+        on = 0,
+        //% blockId="off" block="off"
+        off = 1
+    }
+    
+	//% weight=100
+    //% blockId=motor_MotorRun block="motor|%index|at speed|%PWM"
+    //% PWM.min=-1000 PWM.max=1000
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=3
+    export function motorRun(index: Motors, PWM: number): void {
+        let i2cbuf = pins.createBuffer(3);
+        if (index == 0) {
+            i2cbuf[0] = 0x00;
+            i2cbuf[1] = PWM >> 8;
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
+        if (index == 1) {
+            i2cbuf[0] = 0x02;
+            i2cbuf[1] = PWM >> 8;
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
+        if (index == 2) {	
+            i2cbuf[0] = 0x00;
+            i2cbuf[1] = PWM >> 8;
+            i2cbuf[2] = PWM;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+            i2cbuf[0] = 0x02;
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf);
+        }
+    }
+	
+	//% weight=90
+    //% blockId=sensor_readIR block="read |%SensorID sensor"
+    //% SensorID.fieldEditor="gridpicker" SensorID.fieldOptions.columns=4
+    export function readIR(SensorID: IR_Sensors): number {
+        pins.i2cWriteNumber(
+            N76_ADDR,
+            SensorID,
+            NumberFormat.UInt8LE,
+            false
+        )
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.UInt16BE, false)
+    }
+	
+	//% weight=80
+    //% blockId=LED_Set block="LED|%LedPin|%status"
+    //% LedPin.fieldEditor="gridpicker" LedPin.fieldOptions.columns=1
+    //% status.fieldEditor="gridpicker" status.fieldOptions.columns=1
+	export function LED(LedPin: LEDs,status: LEDswitch): void {
+        if (LedPin == LEDs.LED_R)
+		{
+            pins.digitalWritePin(DigitalPin.P8, status)
+		}
+        else if (LedPin == LEDs.LED_L)
+		{
+            pins.digitalWritePin(DigitalPin.P16, status)
+		}
+    }
+	
+	//%color=#308f60
+	//% weight=40
+	//% blockId=sonar_ping block="read ultrasonic sensor |%unit "
+    export function pingultrasonic_sensor(unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(DigitalPin.P1, PinPullMode.PullNone);
+        pins.digitalWritePin(DigitalPin.P1, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(DigitalPin.P1, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(DigitalPin.P1, 0);
+
+        // read pulse
+        const d = pins.pulseIn(DigitalPin.P2, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            default: return d ;
+        }
+    }
+}
